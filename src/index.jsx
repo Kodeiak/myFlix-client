7fc8ae8c4264169c07626ca01ca30bb7efac3597
Paul Wandrey <88623843+Kodeiak@users.noreply.github.com>
2022-05-11 16:58:19 -0500

    formatting

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { MainView } from "./components/main-view/main-view";
 
 import "./index.scss";
 
+// create main component
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
+      <MainView />
     );
   }
 }
 
+// find the root of the app
 const container = document.getElementsByClassName("app-container")[0];
 
+// render app in root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
